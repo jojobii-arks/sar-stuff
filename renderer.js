8b017fe7fc188b48e23531a3799a9bad512b70e4
Joseph Bravo <josephangelobravo@gmail.com>
2022-05-18 01:04:37 +0000

    testing out node-canvas

--- renderer.js
+++ renderer.js
@@ -1,4 +1,27 @@
-const { createCanvas } = require('canvas');
+const fs = require('fs/promises');
 
-const canvas = createCanvas(400, 400);
-console.dir(canvas);
+let parsed;
+fs.readFile('./output/io-blush-data.json', { encoding: 'utf8' })
+  .then(JSON.parse)
+  .catch(console.error);
+
+const { createCanvas, loadImage } = require('canvas');
+const canvas = createCanvas(200, 200);
+const ctx = canvas.getContext('2d');
+
+// Write "Awesome!"
+ctx.font = '30px Impact';
+ctx.rotate(0.1);
+ctx.fillText('Awesome!', 50, 100);
+
+// Draw line under text
+var text = ctx.measureText('Awesome!');
+ctx.strokeStyle = 'rgba(0,0,0,0.5)';
+ctx.beginPath();
+ctx.lineTo(50, 102);
+ctx.lineTo(50 + text.width, 102);
+ctx.stroke();
+
+const out = canvas.createPNGStream();
+
+fs.writeFile(__dirname + '/test.png', out);
